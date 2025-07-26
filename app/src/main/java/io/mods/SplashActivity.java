fbdce303a255a6fd70009f5a68e535dcb63634df
綾小路 <134218160+Techflux0@users.noreply.github.com>
2025-07-26 09:31:05 +0300

    ---

--- app/src/main/java/io/mods/SplashActivity.java
+++ app/src/main/java/io/mods/SplashActivity.java
@@ -4,11 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 public class SplashActivity extends Activity {
 
@@ -17,25 +12,9 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        RelativeLayout splashLayout = findViewById(R.id.splashLayout);
-        ImageView logo = findViewById(R.id.logo);
-
-        AlphaAnimation fade = new AlphaAnimation(0.0f, 1.0f);
-        fade.setDuration(2000);
-        splashLayout.startAnimation(fade);
-
-        Animation shake = new TranslateAnimation(-10, 10, -10, 10);
-        shake.setDuration(500);
-        shake.setRepeatMode(Animation.REVERSE);
-        shake.setRepeatCount(5);
-        logo.startAnimation(shake);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
-            }
-        }, 3000);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 5000); 
     }
 }
